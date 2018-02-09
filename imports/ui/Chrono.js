242dc926858c69f1cae4854bf309a298fa011804
Ethnolatin <fduchesne@ethnolatin.com>
2018-02-09 23:08:35 +0100

    Ajout bouton temps intermédiaire

--- imports/ui/Chrono.js
+++ imports/ui/Chrono.js
@@ -9,15 +9,24 @@ export default class Chrono extends Component {
     this.state = {
       label: 'Go',
       time: 0,
+      timeDisplay: 0,
       demarre: null,
+      lap: null,
     };
     this.incrementCompteur = this.incrementCompteur.bind(this);
     this.goStopClick = this.goStopClick.bind(this);
+    this.lapClick = this.lapClick.bind(this);
     this.resetClick = this.resetClick.bind(this);
   }
 
   incrementCompteur() {
     this.setState({ time: this.state.time + 1 });
+    if (!this.state.lap) {
+      this.setState ({ timeDisplay: this.state.time });
+    } else {
+      this.setState ({ timeDisplay: this.state.timeDisplay });
+    }
+    console.log(this.state.lap, this.state.time, this.state.timeDisplay);
   }
 
   goStopClick() {
@@ -31,15 +40,25 @@ export default class Chrono extends Component {
     this.state.demarre = !this.state.demarre;
   }
 
+  lapClick() {
+    this.setState({ lap: !this.state.lap });
+    if (this.state.lap) {
+      this.setState ({ timeDisplay: this.state.time });
+    } else {
+      this.setState ({ timeDisplay: this.state.timeDisplay });
+    }
+  }
+
   resetClick() {
-    this.setState({ time: 0 });
+    this.setState({ time: 0, timeDisplay: 0 });
   }
 
   render() {
     return (
       <table><tbody><tr>
-        <td><Time time={new Date(0, 0, 0, 0, 0, Number(this.state.time))}/></td>
+        <td><Time time={new Date(0, 0, 0, 0, 0, Number(this.state.timeDisplay))}/></td>
         <td><button onClick={this.goStopClick}>{this.state.label}</button></td>
+        <td><button onClick={this.lapClick}>Lap</button></td>
         <td><button id="reset" onClick={this.resetClick}>Reset</button></td>
       </tr></tbody></table>
     );
