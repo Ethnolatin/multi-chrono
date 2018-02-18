c1edb98e2107a692302bdb3392e75dc652728a57
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-02-18 12:36:15 +0100

    version Sylvain

--- imports/ui/Chrono.js
+++ imports/ui/Chrono.js
@@ -1,70 +1,21 @@
-import React from 'react';
-import { Component } from 'react';
-import Time from './Time.js';
+import React      from 'react'
+import TimeFormat from 'react-time-format'
 
-export default class Chrono extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      label: 'Go',
-      time: 0,
-      timeDisplay: 0,
-      demarre: null,
-      lap: null,
-    };
-    this.incrementCompteur = this.incrementCompteur.bind(this);
-    this.goStopClick = this.goStopClick.bind(this);
-    this.lapClick = this.lapClick.bind(this);
-    this.resetClick = this.resetClick.bind(this);
-  }
-
-  incrementCompteur() {
-    this.setState({ time: this.state.time + 1 });
-    if (!this.state.lap) {
-      this.setState ({ timeDisplay: this.state.time });
-    } else {
-      this.setState ({ timeDisplay: this.state.timeDisplay });
-    }
-  }
-
-  goStopClick() {
-    if (!this.state.demarre) {
-      this.interval = setInterval(this.incrementCompteur, 1000);
-      this.setState({ label: "Stop" });
-    } else {
-      clearInterval(this.interval);
-      this.setState({ label: "Go" });
-    }
-    this.state.demarre = !this.state.demarre;
-  }
-
-  lapClick() {
-    this.setState({ lap: !this.state.lap });
-    if (this.state.lap) {
-      this.setState ({ timeDisplay: this.state.time });
-    } else {
-      this.setState ({ timeDisplay: this.state.timeDisplay });
-    }
-  }
-
-  resetClick() {
-    this.setState({
-      time: 0,
-      timeDisplay: 0,
-      lap: null
-    });
-  }
-
-  render() {
-    return (
-      <table><tbody><tr>
-        <td><Time time={new Date(0, 0, 0, 0, 0, Number(this.state.timeDisplay))}/></td>
-        <td><button onClick={this.goStopClick}>{this.state.label}</button></td>
-        <td><button onClick={this.lapClick}>Lap</button></td>
-        <td><button id="red" onClick={this.resetClick}>Reset</button></td>
-      </tr></tbody></table>
-    );
-  }
-
+export default function(props) {
+  const {
+    timeDisplay, goStopClickHandler, goStopLabel,
+    lapClickHandler, resetClickHandler
+  } = props
+  const time = new Date(0, 0, 0, 0, 0, Number(timeDisplay))
+  return (
+    <table><tbody><tr>
+      <td>
+        <TimeFormat value={time} format='HH:mm:ss' />
+      </td>
+      <td><button onClick={goStopClickHandler}>{goStopLabel}</button></td>
+      <td><button onClick={lapClickHandler}>Lap</button></td>
+      <td><button className="red-button" onClick={resetClickHandler}>Reset</button></td>
+    </tr></tbody></table>
+  )
 }
--- imports/ui/ChronoList.js
+++ imports/ui/ChronoList.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Component } from 'react';
-import Chrono from './Chrono.js';
+import PropTypes from 'prop-types'
+import React     from 'react'
+import ChronoContainer from './ChronoContainer'
 
-export default class ChronoList extends Component {
-
+export default class ChronoList extends React.Component {
+  // propTypes sert à déclarer les props attendues par ce composant
+  // ça affiche un warning dans la console si ces règles ne sont pas respectées
+  // ça sert surtout de documentation "intelligente"
+  static propTypes = {
+    items:              PropTypes.array.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired,
+  }
   constructor(props) {
-    super(props);
-    this.state = { items: [] };
-    this.deleteClick = this.deleteClick.bind(this);
+    super(props)
   }
-
-  deleteClick(index) {
-    this.setState({items: this.props.items.splice(index,1) });
-  }
+  // attention : on ne doit JAMAIS modifier les props (et le state non plus sauf à utiliser setState)
+  // ils sont "immutables" = non modifiables
+  // c'est pour cette raison que je remonte cette fonction au parent et je la fais passer par les props
+  // (il y aussi une raison c'est que maintenant c'est un "composent" donc il ne manipule pas les données
+  // directement : il demande à un container parent de le faire pour lui)
+  //deleteClick(index) {
+  //  this.setState({items: this.props.items.splice(index,1)})
+  //}
+  // il y a un 2e pb sur cette fonction (mais toujours le même sujet) : splice est une fonction qui
+  // modifie le tableau sur laquelle elle s'applique or comme state doit être immutable
+  // il ne faut pas utiliser ce type de fonctions "mutables"
 
   render() {
     return (
@@ -20,11 +31,18 @@ export default class ChronoList extends Component {
         {this.props.items.map((item, index) => (
           <tr key={item.id}>
             <th>#{index+1}</th>
-            <td><Chrono /></td>
-            <td><button id="black" onClick={() => this.deleteClick(index)}>Delete</button></td>
+            <td><ChronoContainer /></td>
+            <td>
+              <button
+                className="black-button"
+                onClick={() => this.props.deleteClickHandler(index)}
+              >
+                Delete
+              </button>
+            </td>
           </tr>
         ))}
       </tbody></table>
-    );
+    )
   }
 }
